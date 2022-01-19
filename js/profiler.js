e184175dc6ce524cade9fef5fb88fbdfa0c9693d
eattato <eattato0804@gmail.com>
2022-01-19 12:51:51 +0900

    added JS file to prefare server -> client

--- js/profiler.js
+++ js/profiler.js
@@ -2,7 +2,7 @@ var genProfile = function (target, param) {
     let profile = document.createElement("div");
     let profile__left = document.createElement("div");
     let profile__imgFrame = document.createElement("div");
-    let profile__img = document.createElement("img");
+    let profile__image = document.createElement("img");
     let profile__statFrame = document.createElement("span");
     let profile__name = document.createElement("span");
     let profile__status = document.createElement("span");
@@ -10,13 +10,24 @@ var genProfile = function (target, param) {
     let chatinfo__time = document.createElement("span");
     let chatinfo__bubble = document.createElement("span");
     let chatinfo__blank = document.createElement("div");
+    profile.className = "profile";
+    profile__left.className = "profile__left";
+    profile__imgFrame.className = "profile__imgFrame";
+    profile__image.className = "profile__image";
+    profile__statFrame.className = "profile__statFrame";
+    profile__name.className = "profile__name";
+    profile__status.className = "profile__status";
+    chatinfo.className = "chatinfo";
+    chatinfo__time.className = "chatinfo__time";
+    chatinfo__bubble.className = "chatinfo__bubble";
+    chatinfo__blank.className = "chatinfo__blank";
 
     profile.appendChild(profile__left);
     profile__left.appendChild(profile__imgFrame);
     if (param.image != undefined)
     {
-        profile__img.src = param.image;
-        profile__imgFrame.appendChild(profile__img);
+        profile__image.src = param.image;
+        profile__imgFrame.appendChild(profile__image);
     }
     profile__left.appendChild(profile__statFrame);
     if (param.name != undefined)
@@ -26,7 +37,7 @@ var genProfile = function (target, param) {
     }
     if (param.status != undefined)
     {
-        profile__status = param.status;
+        profile__status.innerHTML = param.status;
         profile__statFrame.appendChild(profile__status);
     }
 
